19af55e5a1560d52b0fac855002a7978a75afe23
Abhishek Rajput <abhishekrajput8804@gmail.com>
2025-07-03 14:45:02 +0530

    add reviews

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");  
-const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+ const ExpressError=require("./utils/ExpressError.js");
+
+ const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
 
 
 
@@ -37,92 +37,31 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+
+//root route
 app.get("/",(req,res)=>{
     res.send("hi i am root");
 });
 
-///validate schema and making them middleware
-const validateListing=(req,res,next)=>{
-      let {error}=listingSchema.validate(req.body);
-        if(error){
-            let errMsg=error.details.map((el)=>el.message).join(",");
-            throw new ExpressError(400,errMsg);
-        }else{
-            next();
-        }
-}
 
 
 
 
 
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-   const allListings=await Listing.find({});
-   res.render("listings/index.ejs",{allListings});
-    }));
 
 
 
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-
-})
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 
 
-//show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
 
-})) ;
 
-//create route new route
-app.post("/listings",validateListing,
-    wrapAsync(async(req,res,next)=>{
-        const newListing=new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");    
-}));
 
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-//update route
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-    
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-//delelte route
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-});
 
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"my new villa",
-//         description:"By the beach",
-//         price:1200,
-//         location:"Calungute Goa",
-//         country:"India",
 
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("sucessfull testing");
-
-// });
 
 
 
